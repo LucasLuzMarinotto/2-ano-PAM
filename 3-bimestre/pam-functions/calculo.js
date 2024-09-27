fa6d4165fedaf87e184ed944c9302fbe0419f681
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2024-09-27 07:44:53 -0300

    Add files via upload

---
+++ 3-bimestre/pam-functions/calculo.js
@@ -0,0 +1,24 @@
+
+var n1;
+var n2;
+var resultado
+
+function soma(n1, n2){
+    resultado = n1+n2;
+    return resultado;
+}
+
+function subtracao(n1, n2){
+    resultado = n1-n2;
+    return resultado;
+}
+
+function multiplicacao(n1, n2){
+    resultado = n1*n2;
+    return resultado;
+}
+
+function divisao(n1, n2){
+    resultado = n1/n2;
+    return resultado;
+}
