2b20fbb3923080c453652f019c7a914fcd79c4c5
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2024-10-09 07:51:28 -0300

    Add files via upload

---
+++ 4-bimestre/exercicioGeometria/geometrico.js
@@ -0,0 +1,45 @@
+console.log('Digite um número')
+
+var n = Number;
+
+var h = Number;
+
+var p = Number;
+
+var a = Number;
+
+function formaG(n){
+    switch(n){
+        case 3:
+            console.log('Triângulo')
+            break;
+        case 4:
+            console.log('Quadrado')
+            break;
+        case 5:
+            console.log('Pentágono')
+            break;
+        case 6:
+            console.log('Hexágono')
+            break;
+        case 7:
+            console.log('Heptágono')
+            break;
+        case 8:
+            console.log('Octógono')
+            break;
+        case 9:
+            console.log('Eneágono')
+            break;
+        case 10:
+            console.log('Decágono')
+            break;
+        default:
+            console.log('Forma geométrica não implementada');
+            break;
+    }
+}
+
+function calculoGeomeria(){
+
+}
